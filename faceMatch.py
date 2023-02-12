da0a2a531373fc8a106468b8f3e731161df7baa7
Ehsan QarehAghaji <80197312+EhsanQA@users.noreply.github.com>
2023-02-12 19:54:51 +0330

    Update faceMatch.py

--- faceMatch.py
+++ faceMatch.py
@@ -4,6 +4,15 @@ import cv2
 import copy
 from PIL import Image
 from PIL import ImageDraw
+#feats:
+#right_eyebrow
+#left_eyebrow
+#top_lip
+#bottom_lip
+#nose_tip
+#chin
+#nose_bridge
+
 
 
 def getWebcam():
@@ -32,7 +41,7 @@ def tagFaces(frame, locations):
         tL, bR = getCorners(spot, 4)
         cv2.rectangle(frame, tL, bR, (0, 0, 255), 3)
 
-def featureSwap(frame, feats, feat1,feat2):
+def featureSwap(frame, feats, feat1, feat2):
     if len(feats) == 0:
         return False
     eyeList = []
@@ -43,7 +52,7 @@ def featureSwap(frame, feats, feat1,feat2):
     for eye in eyeList:
         eyeRectList.append(maxAndMin(eye))
     for i in range(len(eyeList)//2):
-        eyeSwap(frame, eyeRectList[i], eyeRectList[i+1])
+        eye_bounding_box(frame, eyeRectList[i], eyeRectList[i+1])
 
 def maxAndMin(featCoords):
     adj = 5
@@ -54,82 +63,21 @@ def maxAndMin(featCoords):
         listY.append(tup[1])
     return [min(listX)-adj,min(listY)-adj,max(listX)+adj,max(listY)+adj]
 
-def eyeSwap(frame, eye1, eye2):
+def eye_bounding_box(frame, eye1, eye2):
     # Coords in the eyes go minx, miny, maxx, maxy
     try:
         for x in range((eye1[2]-eye1[0]) * 4):
             for y in range((eye1[3]-eye1[1]) * 4):
-                firstEye = copy.copy(frame[eye2[1]*4 + y][eye2[0]*4 + x])
-                frame[eye2[1]*4 + y][eye2[0]*4 + x] = frame[eye1[1]*4+y][eye1[0]*4+x]
-                frame[eye1[1]*4 + y][eye1[0]*4 + x] = firstEye
+                frame[eye2[1]*4 + y][eye2[0]*4 + x] = 0
+                frame[eye1[1]*4 + y][eye1[0]*4 + x] = 0
+
     except:
         pass
-
-def editFeat(origPic, smallPic, feats, feature, color, borderSize):
-    try:
-        for person in feats:
-            mark = person[feature]
-            mark = np.multiply(np.array(mark, np.int32).reshape((-1, 1, 2)), 4)
-            cv2.polylines(origPic, [mark], True, color, borderSize)
-    except:
-        pass
-
-def getFace(file):
-    rawFace = face_recognition.load_image_file(file)
-    return face_recognition.face_encodings(rawFace)[0]
-
+    
 def getCorners(fL,n):
     tL = (fL[3]*n, fL[0]*n)
     bR = (fL[1]*n, fL[2]*n)
     return tL, bR
 
-def makeFeatRelative(feature):
-    sumX = 0
-    sumY = 0
-    for coord in feature:
-        sumX += coord[0]
-        sumY += coord[1]
-    avgX = sumX // len(feature)
-    avgY = sumY // len(feature)
-    relativeArray = []
-    for coord in feature:
-        relativeArray.append((coord[0]-avgX,coord[1]-avgY))
-    return (avgX,avgY), relativeArray
-
-getWebcam()
-
-#
-# face = face_recognition.load_image_file("jim.png")
-# img = Image.fromarray(face)
-# d = ImageDraw.Draw(img, 'RGBA')
-# feats = face_recognition.face_landmarks(face_recognition.load_image_file("jim.png"))[0]
-# d.polygon(feats['left_eye'], fill=(0, 0, 255, 255))
-# d.polygon(feats["nose_tip"],fill = (255,0,0,255))
-# d.polygon(feats["nose_bridge"],fill = (255,128,0,255))
-# d.polygon(feats["bottom_lip"],fill = (123,128,0,255))
-# d.polygon(feats["top_lip"],fill = (123,128,0,255))
-# d.polygon(feats["left_eyebrow"],fill = (0,128,100,255))
-#
-# img.show()
-
-
-
-
-
-#me = getFace("myface.png")
-# notMe = getFace("notme.jpg")
-# identify = getFace("whoseface.JPG")
-# results = face_recognition.compare_faces([me,notMe], identify)
-# print(results)
-
-
-
-# # top, right, bottom, left
-# fL = face_recognition.face_locations(face_recognition.load_image_file("myface.png"))
-# print(fL[0][0])
-# tL = (fL[0][3], fL[0][0])
-# bR = (fL[0][1], fL[0][2])
-# cv2.rectangle(face,tL,bR, (0, 0, 255), 2)
-# cv2.imshow("yah",face)
-# cv2.waitKey(0)
-
+if __name__ == "__main__":
+    getWebcam()
